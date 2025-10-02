08edf7e1ceebf8341421b9817823bcc9f721e66f
Theyloac <theyloac@gmail.com>
2025-10-01 23:23:23 -0400

    SCRUM-45-workload-detection.py implemented

---
+++ detectors/credit_hours/__init__.py
@@ -0,0 +1,11 @@
+"""
+Credit Hours Detection Module
+==============================
+
+This module provides detection capabilities for credit hour information
+in syllabus documents.
+"""
+
+from .credit_hours_detection import CreditHoursDetector
+
+__all__ = ['CreditHoursDetector']
---
+++ detectors/credit_hours/tests/__init__.py
@@ -0,0 +1,4 @@
+"""
+Credit Hours Detection Tests
+=============================
+"""
